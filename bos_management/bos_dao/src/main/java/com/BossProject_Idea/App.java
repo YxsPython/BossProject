156b20236455e85655be78113dda9a1898382d27
Uers <Uers@UersdeMacBook-Pro.local>
2018-03-11 17:57:40 +0800

    Initial commit

---
+++ bos_management/bos_dao/src/main/java/com/BossProject_Idea/App.java
@@ -0,0 +1,13 @@
+package com.BossProject_Idea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
